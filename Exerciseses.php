00c4c441956fab0a74f69ce98a7da59291b20157
Cornelius Kasokola <41131476+Cornelius98@users.noreply.github.com>
2025-08-07 10:08:51 -0800

    Submission for exercise 2

---
+++ Exerciseses.php
@@ -0,0 +1,31 @@
+<?php
+Exercise 1: For each of the data types listed below. Write a variable and print to the screen.
+Do this exercise twice, first in a filed named "data_types_1.php" and second file named "data_types_2.php"
+
+1. whole number
+2. float
+3. string 
+4. boolean
+5. array 
+6. std object 
+7. null 
+8. resource 
+9. function
+10. class and class object 
+
+Reference code use in the class
+GitHub: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+?
